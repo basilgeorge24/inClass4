7d2cd6399aa1cef5c2b37236c9c782324ebf663b
Basil George <basilgeorgejosephite@gmail.com>
2023-12-07 13:40:04 -0500

    inital commit

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import React, { useState } from "react";
+
+//any aditional imports
+import Task from "./components/Task";
 
 export default function App() {
+  // input area
+  const [task, setTask] = useState();
+
+  // to store all tasks
+  const [taskItems, setTaskItems] = useState([]);
+
+  // event listener
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  //event listener for deleting task
+  const completeTask = () => {
+    let copyItems = [...taskItems];
+    //remove the specific task as selected by the user
+    copyItems.splice(index, 1);
+    //update original array byu rewrting it wit copied array
+    setTaskItems(copyItems);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* list all the To-Do items */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>My To Do Items</Text>
+        {/* Container for all the tasks to be reneders */}
+        <View style={styles.items}>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => {
+                  completeTask(index);
+                }}
+              >
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+      {/* user input for todo tasks */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Add your to do item here"}
+          value=""
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
